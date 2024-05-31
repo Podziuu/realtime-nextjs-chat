528d3f429960e1409b544dccfdf0a72e9d09eeb8
Bartosz Podemski <bartekpodemski@gmail.com>
2024-05-31 19:32:20 +0200

    Template for signup page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ import TailwindButton from "@/components/ui/tailwindcss-buttons";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen flex-col">
-      <h1 className="text-5xl font-bold">Welcome to realtime chat app!</h1>
+    <main className="flex items-center justify-center min-h-screen flex-col bg-black">
+      <h1 className="text-5xl font-bold text-white">
+        Welcome to realtime chat app!
+      </h1>
       <div className="mt-8 space-x-8">
         <TailwindButton text="Sign up!" link="/signup" />
         <TailwindButton text="Log in!" link="/login" />
---
+++ app/signup/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { GridBackgroundDemo } from "@/components/ui/grid-background";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+const page = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <main className="bg-[#090909] min-h-screen flex justify-center items-center">
+      <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.1] bg-grid-black/[0.3] absolute flex items-center justify-center">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      </div>
+      <div className="max-w-xl bg-black w-full p-8 z-10 border border-gray-700 rounded-lg">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input id="firstname" placeholder="Tyler" type="text" />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
+    </main>
+  );
+};
+
+export default page;
