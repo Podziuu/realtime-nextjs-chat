1e241168e86b04a8b475b3f76b0d60524a528694
Bartosz Podemski <bartekpodemski@gmail.com>
2024-06-10 13:49:34 +0200

    signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -18,6 +18,9 @@ import { GridBackgroundDemo } from "@/components/ui/grid-background";
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
+  email: z.string().email(),
+  password: z.string().min(8),
+  cpassword: z.string().min(8),
 });
 
 const page = () => {
@@ -26,19 +29,23 @@ const page = () => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
+      email: "",
+      password: "",
+      cpassword: "",
     },
   });
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values);
+    if (values.password !== values.cpassword) {
+      // toast a message to user
+      alert("Passwords do not match");
+    }
   }
   return (
     <main className="bg-[#090909] min-h-screen flex justify-center items-center">
       <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.1] bg-grid-black/[0.3] absolute flex items-center justify-center">
-        {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
       <div className="max-w-xl bg-black w-full p-8 z-10 border border-gray-700 rounded-lg">
@@ -51,13 +58,65 @@ const page = () => {
                 <FormItem>
                   <FormLabel>Username</FormLabel>
                   <FormControl>
-                    <Input id="firstname" placeholder="Tyler" type="text" />
+                    <Input placeholder="Tyler" type="text" {...field} />
                   </FormControl>
                   <FormDescription>
                     This is your public display name.
                   </FormDescription>
                   <FormMessage />
                 </FormItem>
+                
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email@com.pl" type="text" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+                
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Tyler" type="password" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+                
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="cpassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Tyler" type="password" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+                
               )}
             />
             <Button type="submit">Submit</Button>
