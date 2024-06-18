2f972c5903afc4a40ca0ec3db49df457fc3c4358
Bartosz Podemski <bartekpodemski@gmail.com>
2024-06-18 14:41:00 +0200

    chat ui

--- app/(root)/chat/page.tsx
+++ app/(root)/chat/page.tsx
@@ -1,4 +1,7 @@
+import { MessageForm } from "@/components/messageForm";
+import { Button } from "@/components/ui/button";
 import { Tabs } from "@/components/ui/tabs";
+import { Textarea } from "@/components/ui/textarea";
 import React from "react";
 
 const tabs = [
@@ -29,7 +32,7 @@ const tabs = [
     title: "Active users",
     value: "active users",
     content: (
-        <div className="w-full overflow-hidden relative h-full p-10 text-white bg-gradient-to-br from-neutral-800 to-neutral-950">
+      <div className="w-full overflow-hidden relative h-full p-10 text-white bg-gradient-to-br from-neutral-800 to-neutral-950">
         <p className="mt-8 text-4xl font-bold">Active users</p>
         <div className="flex flex-col gap-6 mt-16">
           <div>
@@ -49,11 +52,32 @@ const tabs = [
 
 const page = () => {
   return (
-    <section>
-      <div className="w-1/4 h-screen relative ">
+    <section className="flex h-screen">
+      <div className="w-1/4 relative">
         <Tabs tabs={tabs} />
       </div>
-      <div></div>
+      <div className="w-full flex flex-col h-full p-4">
+        <div className="border-b-white border-b w-full p-6">
+          <h4>User 5</h4>
+        </div>
+        <div className="p-4 space-y-8 flex flex-col w-full">
+          {/* // TODO map through messages */}
+          <div className="bg-blue-600 w-fit max-w-80 p-1 rounded-lg">
+            Hello i from poland
+          </div>
+          <div className="self-end bg-blue-600 w-fit max-w-80 p-1 rounded-lg">
+            Hello i from england
+          </div>
+          <div className="bg-blue-600 w-fit max-w-80 p-1 rounded-lg">
+            Hello i from poland
+          </div>
+          <div className="self-end bg-blue-600 w-fit max-w-80 p-1 rounded-lg">
+            gdsa gdsa gdsagdsagdsca gdsahguiklghdsa gdsjakhlgdsa gdsajkghdsakjg gdsaagdsa gdsagdsagdsagsdgdsagdsagsdgsda gdsagdsa
+          </div>
+        </div>
+
+        <MessageForm />
+      </div>
     </section>
   );
 };
---
+++ components/messageForm.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+// import { toast } from "@/components/ui/use-toast"
+
+const FormSchema = z.object({
+    message: z.string().min(1)
+});
+
+export function MessageForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+    // toast({
+    //   title: "You submitted the following values:",
+    //   description: (
+    //     <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+    //       <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+    //     </pre>
+    //   ),
+    // })
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full mt-auto space-y-6">
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Send message</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Enter your text here..."
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">Send Message</Button>
+      </form>
+    </Form>
+  );
+}
