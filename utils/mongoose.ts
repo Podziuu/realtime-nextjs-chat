f37c005e70e35a05b470d849e4a7c08f83ebb7c0
Bartosz Podemski <bartekpodemski@gmail.com>
2024-06-19 15:04:37 +0200

    Add connection to mongoDB

---
+++ utils/mongoose.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+let isConnected: boolean = false;
+
+export const connectToDatabase = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) return console.log("MISSING MONGODB URL");
+
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: process.env.MONGODB_DB,
+    });
+    isConnected = true;
+
+    console.log("MongoDB is conncted");
+  } catch (err) {
+    console.log("MongoDB connection failed", err);
+  }
+};
